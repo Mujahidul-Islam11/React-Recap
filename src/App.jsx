682a7d3d433edd4856f26c2fbc4e5f13caa7bb36
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-02 11:24:30 +0600

    Article section complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import Bookmark from './components/Bookmark'
 function App() {
   return (
     <>
-    <div className='w-[1100px] mx-auto'>
-    <nav className='flex justify-between border-b py-4'>
-    <h3 className='text-4xl font-semibold cursor-pointer'>Knowledge Cafe</h3>
+    <div className='md:w-[1100px] mx-auto'>
+    <nav className='flex justify-between border-b py-6'>
+    <h3 className='text-2xl md:text-4xl font-bold cursor-pointer'>Knowledge Cafe</h3>
     <img className='w-12 h-12 rounded-full cursor-pointer' src="./../public/profile.png" alt="" />
     </nav>
-    <div>
+    <div className='mt-6'>
       <Articles></Articles>
       <Bookmark></Bookmark>
     </div>
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -12,8 +12,24 @@ const Articles = () => {
         <div>
            {
             articles?.map(article => 
-            <div key={article?.id}>
-            
+            <div key={article?.id} className="mb-16 md:w-[700px] ">
+            <img className="mb-4 rounded-lg" src={article?.cover} alt="" />
+            <div className="flex justify-between items-center">
+                <div className="flex gap-4 items-center">
+                    <img className="w-12 h-12" src={article?.author_img} alt="" />
+                    <div>
+                    <h2 className="font-bold text-[16px]">{article?.author}</h2>
+                    <p className="text-gray-600">{article?.posted_date}</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 text-gray-500">
+                    <h2>{article?.reading_time}</h2>
+                    <h2 className="text-xl cursor-pointer"><ion-icon name="bookmark-outline"></ion-icon></h2>
+                </div>
+            </div>
+            <h1 className="text-3xl font-semibold my-4">{article?.title}</h1>
+            <p className="text-gray-500">{article?.hashtags}</p>
+            <button className="underline text-blue-700 mt-4">Mark as read</button>
             </div>)
            } 
         </div>
