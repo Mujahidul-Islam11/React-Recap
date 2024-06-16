b9251e88a23babbae4a71b29fbbbfe18f51ddd87
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-16 11:09:15 +0600

    React router dom installed

--- src/main.jsx
+++ src/main.jsx
@@ -4,31 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import './App.css'
 
-class Timer extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={date: new Date()}
-  }
-  componentDidMount(){
-    this.time = setInterval(() => {
-      this.setState({date: new Date()})
-    }, 1000);
-  }
-  componentWillUnmount(){
-    clearInterval(this.time)
-  }
-  render() {
-    return (
-      <div>
-        Hello {new Date().toLocaleTimeString(this.props.local)}
-      </div>
-    )
-  }
-}
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Timer local="bn-BD"/>
+    <App/>
   </React.StrictMode>,
 )
